b4bcd83125b0e789e2e38e66de6573e5f7ac3b26
chicomy <chongmiw@gmail.com>
2020-09-28 04:03:33 -0400

    modify server ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "18.222.179.44", user: "deploy", roles: %w{app db web}
+server "54.167.85.6", user: "deploy", roles: %w{app db web}
 
 
 # role-based syntax
